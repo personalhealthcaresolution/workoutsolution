d8010251cc966c8ec0988bf8622213480ef0d00b
Thanh Long <thanhianyuen@gmail.com>
2015-06-28 08:49:55 +0700

    - add get accelerometer code

--- WorkoutSolution/WorkoutSolution/TrackerViewController.swift
+++ WorkoutSolution/WorkoutSolution/TrackerViewController.swift
@@ -7,16 +7,37 @@
 //
 
 import UIKit
+import CoreMotion
 
 class TrackerViewController: UIViewController {
+    
+    var currentAccelX: Double = 0;
+    var currentAccelY: Double = 0;
+    var currentAccelZ: Double = 0;
+    var currentMaxAccelX: Double = 0;
+    
+    var motionManager = CMMotionManager()
 
     @IBOutlet weak var enableTracker: UISwitch!
+    @IBOutlet weak var accX: UILabel!
+    @IBOutlet weak var accY: UILabel!
+    @IBOutlet weak var accZ: UILabel!
 
     @IBAction func trackerChangeValue(sender: UISwitch) {
         UIDevice.currentDevice().proximityMonitoringEnabled = enableTracker.on
     }
     
     override func viewDidLoad() {
+        
+        motionManager.accelerometerUpdateInterval = 0.2
+        
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue .currentQueue(), withHandler: {(accelerometerData: CMAccelerometerData!, error: NSError!) -> Void in
+            self.outputAccelerationData(accelerometerData.acceleration)
+            if (error != nil){
+                println("\(error)")
+            }
+        })
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -26,16 +47,15 @@ class TrackerViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func outputAccelerationData(acceleration: CMAcceleration){
+        if (UIDevice.currentDevice().proximityState){
+            accX.text = "\(acceleration.x)"
+            accY.text = "\(acceleration.y)"
+            accZ.text = "\(acceleration.z)"
+            if (fabs(acceleration.x) > fabs(currentMaxAccelX)){
+                currentMaxAccelX = acceleration.x
+            }
+        }
     }
-    */
-
 }
