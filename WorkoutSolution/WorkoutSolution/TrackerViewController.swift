aaff3ba52abcbfaf6caf05d8e892fba6596d06d5
Thanh Long <thanhianyuen@gmail.com>
2015-07-01 00:29:21 +0700

    - add counter

--- WorkoutSolution/WorkoutSolution/TrackerViewController.swift
+++ WorkoutSolution/WorkoutSolution/TrackerViewController.swift
@@ -14,9 +14,10 @@ class TrackerViewController: UIViewController {
     var currentAccelX: Double = 0;
     var currentAccelY: Double = 0;
     var currentAccelZ: Double = 0;
-    var currentMaxAccelX: Double = 0;
+    var currentMaxAccelZ: Double = 0;
+    var counter: Int = 0;
     
-    var motionManager = CMMotionManager()
+    //let motionManager = CMMotionManager()
 
     @IBOutlet weak var enableTracker: UISwitch!
     @IBOutlet weak var accX: UILabel!
@@ -24,45 +25,55 @@ class TrackerViewController: UIViewController {
     @IBOutlet weak var accZ: UILabel!
     @IBOutlet weak var curProximity: UILabel!
     @IBOutlet weak var proximityState: UILabel!
+    
+    lazy var motionManager: CMMotionManager = {
+        let motion = CMMotionManager()
+        motion.accelerometerUpdateInterval = 1.0/10.0
+        return motion
+    }()
 
     @IBAction func trackerChangeValue(sender: UISwitch) {
         UIDevice.currentDevice().proximityMonitoringEnabled = enableTracker.on
         curProximity.text = UIDevice.currentDevice().proximityMonitoringEnabled.description
+        if enableTracker.on {
+            let queue = NSOperationQueue()
+            self.motionManager.startAccelerometerUpdatesToQueue(queue, withHandler:
+                {data, error in
+                    guard let data = data else {
+                        return
+                    }
+                    self.outputAccelerationData(data.acceleration)
+                }
+            )
+        } else {
+            self.motionManager.stopAccelerometerUpdates()
+        }
         proximityState.text = UIDevice.currentDevice().proximityState.description
     }
     
     override func viewDidLoad() {
-        
         curProximity.text = UIDevice.currentDevice().proximityMonitoringEnabled.description
-        proximityState.text = UIDevice.currentDevice().proximityState.description
+        proximityState.text = "0"
         
-        motionManager.accelerometerUpdateInterval = 0.2
-        /*
-        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue .currentQueue(), withHandler: {(accelerometerData: CMAccelerometerData!, error: NSError!) -> Void in
-            self.outputAccelerationData(accelerometerData.acceleration)
-            if (error != nil){
-                println("\(error)")
-            }
-        })
-        */
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     func outputAccelerationData(acceleration: CMAcceleration){
-        if (UIDevice.currentDevice().proximityState){
-            accX.text = "\(acceleration.x)"
-            accY.text = "\(acceleration.y)"
-            accZ.text = "\(acceleration.z)"
-            if (fabs(acceleration.x) > fabs(currentMaxAccelX)){
-                currentMaxAccelX = acceleration.x
+        //if (UIDevice.currentDevice().proximityState){
+            self.accX.text = "\(acceleration.x)"
+            self.accY.text = "\(acceleration.y)"
+            self.accZ.text = "\(acceleration.z)"
+            print("X = \(acceleration.x)")
+            print("Y = \(acceleration.y)")
+            print("Z = \(acceleration.z)")
+            if (fabs(acceleration.z) > fabs(currentMaxAccelZ)){
+                counter++;
+                proximityState.text = counter.description
             }
-        }
+        //}
     }
 }
