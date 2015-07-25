ac8829d053c7e98506e33ccdef2d5016b0757ef1
Thanh Long Nguyen <ianyuen@khais-mac-mini>
2015-07-25 18:32:33 +0700

    - add Facebook Login

---
+++ WorkoutSolution/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  WorkoutSolution
+//
+//  Created by Khai Dao on 6/10/15.
+//  Copyright (c) 2015 Khai Dao. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error == nil {
+            print("login complete")
+            self.performSegueWithIdentifier("showApp", sender: self)
+        } else {
+            print(error.localizedDescription)
+        }
+    }
+
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("logged out")
+    }
+
+}
+
