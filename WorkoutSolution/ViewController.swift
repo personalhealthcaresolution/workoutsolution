8f17d053515e062f6822559c7f63b87b2d5f6e39
ianyuen <thanhianyuen@gmail.com>
2015-10-25 11:33:33 +0700

    - move local notification to Settings.swift

--- WorkoutSolution/ViewController.swift
+++ WorkoutSolution/ViewController.swift
@@ -20,12 +20,6 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         loginButton.center = self.view.center
         loginButton.delegate = self
         self.view.addSubview(loginButton)
-
-        let localNotification: UILocalNotification = UILocalNotification()
-        localNotification.alertAction = "notifications on"
-        localNotification.alertBody = "Woww it works!!"
-        localNotification.fireDate = NSDate(timeIntervalSinceNow: 30)
-        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
     }
     
     override func viewDidAppear(animated: Bool) {
--- WorkoutSolution/ViewController/Settings.swift
+++ WorkoutSolution/ViewController/Settings.swift
@@ -14,6 +14,34 @@ class Settings: UIViewController {
     @IBAction func notificationChangedValue(sender: AnyObject) {
         let defaults = NSUserDefaults.standardUserDefaults()
         defaults.setBool(enableNotification.on, forKey: "enableNotification")
+        
+        if (enableNotification.on) {
+            
+            let curDate = NSDate()
+            let calendar = NSCalendar.currentCalendar()
+            let componentsYear = calendar.components(NSCalendarUnit.Year, fromDate: curDate)
+            let componentsMonth = calendar.components(NSCalendarUnit.Month, fromDate: curDate)
+            let componentsDay = calendar.components(NSCalendarUnit.Day, fromDate: curDate)
+            
+            let dateComp:NSDateComponents = NSDateComponents()
+            dateComp.year = componentsYear.year
+            dateComp.month = componentsMonth.month;
+            dateComp.day = componentsDay.day;
+            dateComp.hour = 05;
+            dateComp.minute = 30;
+            dateComp.timeZone = NSTimeZone.systemTimeZone()
+            
+            let calender:NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+            let date:NSDate = calender.dateFromComponents(dateComp)!
+            
+            let notification:UILocalNotification = UILocalNotification()
+            notification.category = "notifications on"
+            notification.alertBody = "Woww it works!!"
+            notification.fireDate = date
+            notification.repeatInterval = NSCalendarUnit.Day
+                
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        }
     }
     
     override func viewDidLoad() {
@@ -25,6 +53,5 @@ class Settings: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
