9f4b1e5fc89de880625660030e5985721f73b09a
ianyuen <thanhianyuen@gmail.com>
2015-11-12 07:58:57 +0700

    + add bridge file

---
+++ WorkoutSolution/WorkoutSolution-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  WorkoutSolution-Bridging-Header.h
+//  WorkoutSolution
+//
+//  Created by Thanh Long Nguyen on 11/12/15.
+//  Copyright © 2015 Khai Dao. All rights reserved.
+//
+
+#ifndef WorkoutSolution_Bridging_Header_h
+#define WorkoutSolution_Bridging_Header_h
+
+ #import "CorePlot-CocoaTouch.h"
+
+#endif /* WorkoutSolution_Bridging_Header_h */
