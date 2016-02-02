5ebc9c2d0d4cb7c40c9ac18f97032e5a2cb862fc
ianyuen <thanhianyuen@gmail.com>
2016-02-02 20:25:30 +0700

    - draw first line charts

--- WorkoutSolution/ViewController/History.swift
+++ WorkoutSolution/ViewController/History.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import Charts
 
 class History: UIViewController {
+    @IBOutlet weak var barChartView: BarChartView!
     
-    var datesInWeek :[NSNumber] = [10, 35, 100, 20, 50, 5]
+    var months: [String]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        months = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ]
+        let unitsSold = [ 20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0 ]
+        setChart(months, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart."
+        barChartView.descriptionText = ""
+        barChartView.noDataTextDescription = "GIVE REASON"
+        
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0 ..< dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+        barChartView.data = chartData
     }
 }
