c28d76ae331b907a655da3a479be1d0c6244bd5e
ianyuen <thanhianyuen@gmail.com>
2016-05-10 02:15:50 +0700

    + MainNew.storyboard (for new design)

---
+++ WorkoutSolution/ViewController/Main.swift
@@ -0,0 +1,35 @@
+//
+//  Main.swift
+//  WorkoutSolution
+//
+//  Created by Thanh Long Nguyen on 5/10/16.
+//  Copyright © 2016 Khai Dao. All rights reserved.
+//
+
+import UIKit
+
+class Main: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
