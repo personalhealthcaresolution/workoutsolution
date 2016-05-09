ee8d313d876fc92ed07d6d171569d33275bdbe86
ianyuen <thanhianyuen@gmail.com>
2016-05-10 02:53:00 +0700

    - set background color

---
+++ WorkoutSolution/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  WorkoutSolution
+//
+//  Created by Thanh Long Nguyen on 5/10/16.
+//  Copyright © 2016 Khai Dao. All rights reserved.
+//
+
+import Foundation
+
+class Color {
+    internal static let coralRed:UInt32 = 0xF94343
+
+    class func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8) / 256.0
+        let blue = CGFloat(rgbValue & 0xFF) / 256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+}
