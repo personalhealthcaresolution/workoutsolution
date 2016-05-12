4ffe5da0f925855b1ef0b8f38ec18fc62e5860cd
ianyuen <thanhianyuen@gmail.com>
2016-05-12 20:40:40 +0700

    - use bold font

---
+++ WorkoutSolution/Constant/Font.swift
@@ -0,0 +1,13 @@
+//
+//  Font.swift
+//  WorkoutSolution
+//
+//  Created by Thanh Long Nguyen on 5/12/16.
+//  Copyright © 2016 Khai Dao. All rights reserved.
+//
+
+import Foundation
+
+class Font {
+    internal static let helveticaNeueBold:String = "HelveticaNeue-Bold"
+}
--- WorkoutSolution/ViewController/Main.swift
+++ WorkoutSolution/ViewController/Main.swift
@@ -100,7 +100,7 @@ class Main: UIViewController {
         let labelType = UILabel()
         labelType.frame = CGRectMake(xPosition, yPosition, width, height)
         labelType.text = "TYPE"
-        labelType.font = UIFont(name: "Arial", size: 20)
+        labelType.font = UIFont(name: Font.helveticaNeueBold, size: 20)
         labelType.textColor = Color.UIColorFromHex(Color.white)
         self.view.addSubview(labelType)
 
@@ -120,7 +120,7 @@ class Main: UIViewController {
         let labelLevel = UILabel()
         labelLevel.frame = CGRectMake(xPosition, yPosition, width, height)
         labelLevel.text = "LEVEL"
-        labelType.font = UIFont(name: "Arial", size: 20)
+        labelLevel.font = UIFont(name: Font.helveticaNeueBold, size: 20)
         labelLevel.textColor = Color.UIColorFromHex(Color.white)
         self.view.addSubview(labelLevel)
     }
