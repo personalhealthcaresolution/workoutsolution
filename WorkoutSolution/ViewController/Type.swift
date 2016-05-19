e712f089c367e8fe1e10b7de145ea74a7342be58
ianyuen <thanhianyuen@gmail.com>
2016-05-19 22:59:45 +0700

    - add background for Type view controller

--- WorkoutSolution/ViewController/Type.swift
+++ WorkoutSolution/ViewController/Type.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class Type: UIViewController {
+    var screenWidth: CGFloat = 0
+    var screenHeight: CGFloat = 0
+    let statusHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        screenWidth = self.view.frame.size.width
+        screenHeight = self.view.frame.size.height
+        initView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        let swap = screenWidth
+        screenWidth = screenHeight
+        screenHeight = swap
+        initView()
+    }
+
+    func initView() {
+        var width: CGFloat = 0
+        var height: CGFloat = 0
+        var xPosition: CGFloat = 0
+        var yPosition: CGFloat = 0
+        
+        width = screenWidth
+        height = screenHeight - statusHeight
+        xPosition = 0
+        yPosition = statusHeight
+        let background = UILabel()
+        background.frame = CGRectMake(xPosition, yPosition, width, height)
+        background.backgroundColor = Color.UIColorFromHex(Color.citrus)
+        self.view.addSubview(background)
     }
 }
