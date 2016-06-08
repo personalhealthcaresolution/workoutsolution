25bb03f75d6bd05c8f46672f3b293d60efc3f60f
ianyuen <thanhianyuen@gmail.com>
2016-06-08 18:52:12 +0700

    - back button for Level view controller

--- WorkoutSolution/ViewController/Level.swift
+++ WorkoutSolution/ViewController/Level.swift
@@ -31,13 +31,35 @@ class Level: UIViewController {
         var xPosition: CGFloat = 0
         var yPosition: CGFloat = 0
 
-        width = screenWidth
-        height = screenHeight - statusHeight
+        var positionX: CGFloat = 0
+        var positionY: CGFloat = 0
+        var itemWidth: CGFloat = 0
+        var itemHeight: CGFloat = 0
+
+        width = 1080
+        height = 1920
         xPosition = 0
-        yPosition = statusHeight
+        yPosition = 80
+        positionX = ScreenSize.getPositionX(screenWidth, positionX: xPosition)
+        positionY = ScreenSize.getPositionY(screenHeight, positionY: yPosition)
+        itemWidth = ScreenSize.getItemWidth(screenWidth, itemWidth: width)
+        itemHeight = ScreenSize.getItemHeight(screenHeight, itemHeight: height)
         let background = UILabel()
-        background.frame = CGRectMake(xPosition, yPosition, width, height)
+        background.frame = CGRectMake(positionX, positionY, itemWidth, itemHeight)
         background.backgroundColor = Color.UIColorFromHex(Color.citrus)
         self.view.addSubview(background)
+        
+        width = 120
+        height = 120
+        xPosition = 60
+        yPosition = 80
+        positionX = ScreenSize.getPositionX(screenWidth, positionX: xPosition)
+        positionY = ScreenSize.getPositionY(screenHeight, positionY: yPosition)
+        itemWidth = ScreenSize.getItemWidth(screenWidth, itemWidth: width)
+        itemHeight = ScreenSize.getItemHeight(screenHeight, itemHeight: height)
+        let btnBack = UIButton()
+        btnBack.frame = CGRectMake(positionX, positionY, itemWidth, itemHeight)
+        btnBack.setImage(UIImage(named: "back"), forState: UIControlState.Normal)
+        self.view.addSubview(btnBack)
     }
 }
