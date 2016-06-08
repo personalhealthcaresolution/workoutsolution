9533222936c9bb33762d096d3a29a74cd9327e48
ianyuen <thanhianyuen@gmail.com>
2016-06-08 18:49:49 +0700

    - background for Level view controller

--- WorkoutSolution/ViewController/Level.swift
+++ WorkoutSolution/ViewController/Level.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class Level: UIViewController {
+    var screenWidth: CGFloat = 0
+    var screenHeight: CGFloat = 0
+    let statusHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        screenWidth = self.view.frame.size.width
+        screenHeight = self.view.frame.size.height
+        initView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func initView() {
+        var width: CGFloat = 0
+        var height: CGFloat = 0
+        var xPosition: CGFloat = 0
+        var yPosition: CGFloat = 0
+
+        width = screenWidth
+        height = screenHeight - statusHeight
+        xPosition = 0
+        yPosition = statusHeight
+        let background = UILabel()
+        background.frame = CGRectMake(xPosition, yPosition, width, height)
+        background.backgroundColor = Color.UIColorFromHex(Color.citrus)
+        self.view.addSubview(background)
     }
 }
