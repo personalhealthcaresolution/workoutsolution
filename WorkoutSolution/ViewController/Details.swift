5e39609d03fa8e514443ddfa2bd957dc3f78a5bd
ianyuen <thanhianyuen@gmail.com>
2016-07-24 14:25:59 +0700

    + Details view controller

---
+++ WorkoutSolution/ViewController/Details.swift
@@ -0,0 +1,64 @@
+//
+//  Details.swift
+//  WorkoutSolution
+//
+//  Created by Thanh Long Nguyen on 7/24/16.
+//  Copyright © 2016 Khai Dao. All rights reserved.
+//
+
+import UIKit
+
+class Details: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ScreenSize.setStatusHeight(UIApplication.sharedApplication().statusBarFrame.size.height)
+        ScreenSize.setCurrentWidth(self.view.frame.size.width)
+        ScreenSize.setCurrentHeight(self.view.frame.size.height)
+        initView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        let swap = ScreenSize.getCurrentWidth()
+        ScreenSize.setCurrentWidth(ScreenSize.getCurrentHeight())
+        ScreenSize.setCurrentHeight(swap)
+        //initView()
+    }
+
+    func initView() {
+        ScreenObject.addBackground(self, xPosition: 0, yPosition: 83, width: ScreenSize.defaultWidth, height: ScreenSize.defaultHeight, color: Color.citrus)
+        ScreenObject.addButton(self, xPosition: 70, yPosition: 110, width: 50, height: 90, icon: "back", selector: #selector(Details.btnBackClicked(_:)))
+
+        ScreenObject.addBackground(self, xPosition: 0, yPosition: 223, width: ScreenSize.defaultWidth, height: 709, color: Color.coralRed)
+
+        ScreenObject.addImage(self, xPosition: 76, yPosition: 386, width: 385, height: 400, named: "before")
+        ScreenObject.addLabel(self, xPosition: 93, yPosition: 829, width: 350, height: 57, text: "BEFORE", font: Font.helveticaNeueBold, size: 22, color: Color.citrus)
+
+        ScreenObject.addImage(self, xPosition: 536, yPosition: 273, width: 630, height: 510, named: "behind")
+        ScreenObject.addLabel(self, xPosition: 685, yPosition: 829, width: 332, height: 57, text: "BEHIND", font: Font.helveticaNeueBold, size: 22, color: Color.citrus)
+
+        ScreenObject.addBackground(self, xPosition: 0, yPosition: 1813, width: ScreenSize.defaultWidth, height: 255, color: Color.coralRed)
+        ScreenObject.addButton(self, xPosition: 260, yPosition: 1866, width: 722, height: 149, icon: "button")
+
+        ScreenObject.addButton(self, xPosition: 112, yPosition: 2076, width: 90, height: 90, icon: "exercises")
+        ScreenObject.addLabel(self, xPosition: 112, yPosition: 2166, width: 320, height: 30, text: "Exercises", font: Font.helveticaNeueBold, size: 5, color: Color.white)
+        
+        ScreenObject.addButton(self, xPosition: 412, yPosition: 2076, width: 90, height: 90, icon: "workouts")
+        ScreenObject.addLabel(self, xPosition: 412, yPosition: 2166, width: 320, height: 30, text: "Workouts", font: Font.helveticaNeueBold, size: 5, color: Color.white)
+        
+        ScreenObject.addButton(self, xPosition: 712, yPosition: 2076, width: 90, height: 90, icon: "tracker")
+        ScreenObject.addLabel(self, xPosition: 722, yPosition: 2166, width: 320, height: 30, text: "Tracker", font: Font.helveticaNeueBold, size: 5, color: Color.white)
+        
+        ScreenObject.addButton(self, xPosition: 1012, yPosition: 2076, width: 90, height: 90, icon: "setting")
+        ScreenObject.addLabel(self, xPosition: 1022, yPosition: 2166, width: 320, height: 30, text: "Settings", font: Font.helveticaNeueBold, size: 5, color: Color.white)
+    }
+
+    func btnBackClicked(sender:UIButton!) {
+        self.performSegueWithIdentifier("showWorkout", sender: self)
+    }
+}
--- WorkoutSolution/ViewController/WorkoutList.swift
+++ WorkoutSolution/ViewController/WorkoutList.swift
@@ -31,27 +31,27 @@ class WorkoutList: UIViewController {
     }
 
     func initView() {
-        ScreenObject.addBackground(self, xPosition: 0, yPosition: 80, width: ScreenSize.defaultWidth, height: ScreenSize.defaultHeight, color: Color.citrus)
-        ScreenObject.addButton(self, xPosition: 70, yPosition: 83, width: 140, height: 140, icon: "back", selector: #selector(WorkoutList.btnBackClicked(_:)))
+        ScreenObject.addBackground(self, xPosition: 0, yPosition: 83, width: ScreenSize.defaultWidth, height: ScreenSize.defaultHeight, color: Color.citrus)
+        ScreenObject.addButton(self, xPosition: 70, yPosition: 110, width: 50, height: 90, icon: "back", selector: #selector(WorkoutList.btnBackClicked(_:)))
 
         ScreenObject.addBackground(self, xPosition: 0, yPosition: 220, width: ScreenSize.defaultWidth, height: 344, color: Color.coralRed)
-        ScreenObject.addButton(self, xPosition: 100, yPosition: 240, width: 304, height: 304, icon: "chinups")
+        ScreenObject.addButton(self, xPosition: 100, yPosition: 240, width: 304, height: 304, icon: "chinups", selector: #selector(WorkoutList.btnDetailsClicked(_:)))
         ScreenObject.addLabel(self, xPosition: 440, yPosition: 310, width: 280, height: 164, text: "CHINUP", font: Font.helveticaNeueBold, size: 18, color: Color.citrus)
 
         ScreenObject.addBackground(self, xPosition: 0, yPosition: 595, width: ScreenSize.defaultWidth, height: 344, color: Color.coralRed)
-        ScreenObject.addButton(self, xPosition: 100, yPosition: 615, width: 304, height: 304, icon: "wallSix")
+        ScreenObject.addButton(self, xPosition: 100, yPosition: 615, width: 304, height: 304, icon: "wallSix", selector: #selector(WorkoutList.btnDetailsClicked(_:)))
         ScreenObject.addLabel(self, xPosition: 440, yPosition: 685, width: 350, height: 164, text: "WALL SIX", font: Font.helveticaNeueBold, size: 18, color: Color.citrus)
 
         ScreenObject.addBackground(self, xPosition: 0, yPosition: 970, width: ScreenSize.defaultWidth, height: 344, color: Color.coralRed)
-        ScreenObject.addButton(self, xPosition: 100, yPosition: 990, width: 304, height: 304, icon: "dipOnChair")
+        ScreenObject.addButton(self, xPosition: 100, yPosition: 990, width: 304, height: 304, icon: "dipOnChair", selector: #selector(WorkoutList.btnDetailsClicked(_:)))
         ScreenObject.addLabel(self, xPosition: 440, yPosition: 1060, width: 500, height: 164, text: "DIP ON CHAIR", font: Font.helveticaNeueBold, size: 18, color: Color.citrus)
 
         ScreenObject.addBackground(self, xPosition: 0, yPosition: 1345, width: ScreenSize.defaultWidth, height: 344, color: Color.coralRed)
-        ScreenObject.addButton(self, xPosition: 100, yPosition: 1365, width: 304, height: 304, icon: "squats")
+        ScreenObject.addButton(self, xPosition: 100, yPosition: 1365, width: 304, height: 304, icon: "squats", selector: #selector(WorkoutList.btnDetailsClicked(_:)))
         ScreenObject.addLabel(self, xPosition: 440, yPosition: 1435, width: 280, height: 164, text: "SQUAT", font: Font.helveticaNeueBold, size: 18, color: Color.citrus)
 
         ScreenObject.addBackground(self, xPosition: 0, yPosition: 1720, width: ScreenSize.defaultWidth, height: 344, color: Color.coralRed)
-        ScreenObject.addButton(self, xPosition: 100, yPosition: 1740, width: 304, height: 304, icon: "pushUp")
+        ScreenObject.addButton(self, xPosition: 100, yPosition: 1740, width: 304, height: 304, icon: "pushUp", selector: #selector(WorkoutList.btnDetailsClicked(_:)))
         ScreenObject.addLabel(self, xPosition: 440, yPosition: 1810, width: 320, height: 164, text: "PUSH UP", font: Font.helveticaNeueBold, size: 18, color: Color.citrus)
 
         ScreenObject.addButton(self, xPosition: 112, yPosition: 2076, width: 90, height: 90, icon: "exercises")
@@ -65,6 +65,10 @@ class WorkoutList: UIViewController {
 
         ScreenObject.addButton(self, xPosition: 1012, yPosition: 2076, width: 90, height: 90, icon: "setting")
         ScreenObject.addLabel(self, xPosition: 1022, yPosition: 2166, width: 320, height: 30, text: "Settings", font: Font.helveticaNeueBold, size: 5, color: Color.white)
+    }
+
+    func btnDetailsClicked(sender:UIButton!) {
+        self.performSegueWithIdentifier("showDetails", sender: self)
     }
 
     func btnBackClicked(sender:UIButton!) {
