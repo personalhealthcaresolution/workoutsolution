a4bc224807e1a760ae538657ab3b36003aa9ed8f
ianyuen <thanhianyuen@gmail.com>
2016-08-17 22:26:29 +0700

    - use xml to draw Type view controller

--- WorkoutSolution/ViewController/Type.swift
+++ WorkoutSolution/ViewController/Type.swift
@@ -31,26 +31,43 @@ class Type: UIViewController {
     }
 
     func initView() {
-        let constant = Constant()
+        var objects = [ScreenObject.Object()]
         let screenObject = ScreenObject()
+        objects = screenObject.getObjects("type")
+        
+        let i: Int = 0
+        while  i < objects.count {
+            var object = ScreenObject.Object()
+            object = objects.first!
+            switch object.type {
+            case "background":
+                screenObject.addBackground(self, xPosition: object.xPosition, yPosition: object.yPosition, width: object.width, height: object.height, color: object.color)
+            case "button":
+                screenObject.addButton(self, xPosition: object.xPosition, yPosition: object.yPosition, width: object.width, height: object.height, icon: object.icon, selector: getSelector(object.selector))
+            case "image":
+                screenObject.addImage(self, xPosition: object.xPosition, yPosition: object.yPosition, width: object.width, height: object.height, named: object.named)
+            case "label":
+                screenObject.addLabel(self, xPosition: object.xPosition, yPosition: object.yPosition, width: object.width, height: object.height, text: object.text, font: object.font, size: object.size, color: object.color)
+            default:
+                break
+            }
+            objects.removeFirst()
+        }
+    }
 
-        screenObject.addBackground(self, xPosition: 0, yPosition: 83, width: ScreenSize.defaultWidth, height: ScreenSize.defaultHeight, color: constant.citrus)
-        screenObject.addButton(self, xPosition: 70, yPosition: 110, width: 50, height: 90, icon: "back", selector: #selector(Type.btnBackClicked(_:)))
-
-        screenObject.addBackground(self, xPosition: 0, yPosition: 223, width: ScreenSize.defaultWidth, height: 380, color: constant.coralRed)
-        screenObject.addButton(self, xPosition: 104, yPosition: 262, width: 302, height: 302, icon: "upper", selector: #selector(Type.btnUpperClicked(_:)))
-        screenObject.addLabel(self, xPosition: 536, yPosition: 389, width: 619, height: 48, text: "UPPER BODY", font: constant.helveticaNeueBold, size: 15, color: constant.citrus)
-
-        screenObject.addBackground(self, xPosition: 0, yPosition: 638, width: ScreenSize.defaultWidth, height: 380, color: constant.coralRed)
-        screenObject.addButton(self, xPosition: 104, yPosition: 677, width: 302, height: 302, icon: "lower", selector: #selector(Type.btnLowerClicked(_:)))
-        screenObject.addLabel(self, xPosition: 536, yPosition: 804, width: 619, height: 48, text: "LOWER BODY", font: constant.helveticaNeueBold, size: 15, color: constant.citrus)
-
-        screenObject.addBackground(self, xPosition: 0, yPosition: 1053, width: ScreenSize.defaultWidth, height: 380, color: constant.coralRed)
-        screenObject.addButton(self, xPosition: 104, yPosition: 1092, width: 302, height: 302, icon: "coreAbs", selector: #selector(Type.btnCoreAbsClicked(_:)))
-        screenObject.addLabel(self, xPosition: 536, yPosition: 1219, width: 619, height: 48, text: "CORE ABS", font: constant.helveticaNeueBold, size: 15, color: constant.citrus)
-
-        screenObject.addLabel(self, xPosition: 104, yPosition: 1783, width: 391, height: 75, text: "CARDIO", font: constant.helveticaNeueBold, size: 25, color: constant.white)
-        screenObject.addButton(self, xPosition: 592, yPosition: 1547, width: 547, height: 547, icon: "cardio")
+    func getSelector(value: String) -> Selector {
+        switch value {
+        case "btnBackClicked":
+            return #selector(Type.btnBackClicked(_:))
+        case "btnUpperClicked":
+            return #selector(Type.btnUpperClicked(_:))
+        case "btnLowerClicked":
+            return #selector(Type.btnLowerClicked(_:))
+        case "btnCoreAbsClicked":
+            return #selector(Type.btnCoreAbsClicked(_:))
+        default:
+            return nil
+        }
     }
 
     func btnBackClicked(sender:UIButton!) {
