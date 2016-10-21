486fcdac69bed1f709cbf539546a01f24ef39c1f
ianyuen <thanhianyuen@gmail.com>
2016-10-22 01:46:28 +0700

    add ExercisesListCell.swift

---
+++ WorkoutSolution/src/ExercisesListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExercisesListCell.swift
+//  WorkoutSolution
+//
+//  Created by Thanh Long Nguyen on 10/22/16.
+//  Copyright © 2016 Khai Dao. All rights reserved.
+//
+
+import UIKit
+
+class ExercisesListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
